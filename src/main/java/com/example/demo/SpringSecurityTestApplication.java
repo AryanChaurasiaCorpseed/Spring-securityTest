562fe91ebf5bf51cbb97e37d050b069c5f57af72
corpseedBackend <ajay.kumar@corpseed.com>
2023-08-03 18:50:24 +0530

    Second Commit\

---
+++ src/main/java/com/example/demo/SpringSecurityTestApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+
+https://www.bezkoder.com/spring-boot-jwt-authentication/
+	
+	
+	
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTestApplication.class, args);
+	}
+
+}
