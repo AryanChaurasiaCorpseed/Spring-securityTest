f3ce52027318a790dae70c53e14dbf80a9329013
corpseedBackend <ajay.kumar@corpseed.com>
2023-08-14 15:44:26 +0530

    some changes need

--- src/main/java/com/example/demo/security/jwt/JwtUtils.java
+++ src/main/java/com/example/demo/security/jwt/JwtUtils.java
@@ -27,7 +27,7 @@ public class JwtUtils {
   private int jwtExpirationMs;
 
   public String generateJwtToken(Authentication authentication) {
-
+System.out.println("uspppppsp");
     UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
     return Jwts.builder()
