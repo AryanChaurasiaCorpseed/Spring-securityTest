33f195a1723cdf5ce8780643003d75b9111ca52b
corpseedBackend <ajay.kumar@corpseed.com>
2023-09-04 19:12:40 +0530

    add some documentation

--- src/main/java/com/example/demo/SpringSecurityTestApplication.java
+++ src/main/java/com/example/demo/SpringSecurityTestApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 
 
 //https://www.bezkoder.com/spring-boot-jwt-authentication/
-	
+// new documentation added 
+//https://www.youtube.com/watch?v=kn5h-GTjDY0&list=PLSVW22jAG8pBnhAdq9S8BpLnZ0_jVBj0c&index=6
 	
 	
 import org.springframework.boot.autoconfigure.SpringBootApplication;
